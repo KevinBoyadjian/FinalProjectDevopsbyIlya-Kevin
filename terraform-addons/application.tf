415f5753e08bd24a690876d83f1e507aee1a46ea
Junior developer <junior@start-up.com>
2026-05-26 17:46:06 +0300

    Removing an empty char in line 17

--- terraform-addons/application.tf
+++ terraform-addons/application.tf
@@ -15,18 +15,21 @@ resource "null_resource" "football_app_ingress" {
 
   provisioner "local-exec" {
     command = <<EOT
-      # Update kubeconfig to ensure we're targeting the correct cluster
-      aws eks update-kubeconfig --name ${data.terraform_remote_state.core.outputs.cluster_name} --region us-east-1
+    # Update kubeconfig to ensure we're targeting the correct cluster
+aws eks update-kubeconfig --name ${data.terraform_remote_state.core.outputs.cluster_name} --region us-east-1
       
-      # Render the template and apply it
-      kubectl apply -f - <<EOF
-      ${templatefile("../k8s/ingress.tpl", { # Relative path to ingress.tpl
-        hostname            = "api.top5score.com",
-        secret_header_value = var.alb_handshake_secret
-      })}
-      EOF
-    EOT
-    working_dir = path.cwd # Execute from terraform-addons directory
+# Render the template and apply it
+kubectl apply -f - <<EOF
+${templatefile("../k8s/ingress.tpl", { 
+# Relative path to ingress.tpl
+  hostname            = "api.top5score.com",
+  secret_header_value = var.alb_handshake_secret
+})}
+EOF
+EOT
+
+    working_dir = path.cwd 
+    # Execute from terraform-addons directory
   }
 
   # This ensures the Ingress is applied only after ExternalDNS and LB Controller are fully functional
